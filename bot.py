41ac2bab8ea416dda04512805f3b1fa46594bf65
mahmood <alsalami1997m@gmail.com>
2026-04-03 01:23:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from telegram.ext import (
 import yt_dlp
 
 # ---------------- CONFIG ----------------
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
 ADMIN_ID = int(os.getenv('ADMIN_ID', '0'))
 
 if not BOT_TOKEN:
